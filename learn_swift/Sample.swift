ca55426695c69c56d0a9978bcf158ac231d6071b
reer <pigzero7@gmail.com>
2020-07-18 15:21:56 +0900

    Sample.swiftの変更

--- learn_swift/Sample.swift
+++ learn_swift/Sample.swift
@@ -15,18 +15,20 @@
 import Moya
 
 enum Sample {
-    case getHealth(request: [String: Any])
+  case getHealth(bmi: String, suitable_weight:  String, degree_of_obesity: String)
 }
 
 //指定したAPIにアクセスする
 extension Sample: TargetType {
     var baseURL: URL { return URL(string: "https://us-central1-tub-89275.cloudfunctions.net/api_docs")! }
-    var path: String {
-        switch self {
-        case .getHealth:
-            return "/api_docs"
-        }
     }
+
+//パス指定する
+  var path: String {
+    switch self {
+    case .getHealth:
+        return "/api_docs"
+  }
   
   //HTTPメソッド(POST)に設定している
     var method: Moya.Method {
@@ -35,21 +37,26 @@ extension Sample: TargetType {
             return .post
         }
     }
-
-  //リクエストを送る
-    var task: Task {
-        switch self {
-        case .getHealth(let request):
-            return .requestParameters(parameters: request, encoding: URLEncoding.default)
-        }
-    }
-
-    var headers: [String: String]? { return ["content-type": "application/json"] }
-
-    // テストの時などに、実際にAPIを叩かずにローカルのjsonファイルを読み込める
+    
+  // スタブデータ
     var sampleData: Data {
         let path = Bundle.main.path(forResource: "samples", ofType: "json")!
         return FileHandle(forReadingAtPath: path)!.readDataToEndOfFile()
     }
+
+
+  //リクエストパラメータ等
+    var task: Task {
+        switch self {
+        case .getHealth(let bmi, let suitable_weight, let degree_of_obesity):
+          return .requestParameters(parameters: ["bmi" : bmi, "suitable_weight" : suitable_weight, "degree_of_obesity" : degree_of_obesity], encoding: URLEncoding.default)
+        }
+    }
+    
+  //ヘッダー
+    var headers: [String: String]? {
+      return nil
+    }
+
 }
 
